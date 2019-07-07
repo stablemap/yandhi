444c72e131ca7f0c630e003d8ee9f3f8d886e8ff
Dylan Moreland <stablemap@icloud.com>
2019-07-07 07:38:51 -0400

    Toy implementation of POST /shoes

--- routes/shoes.js
+++ routes/shoes.js
@@ -5,4 +5,11 @@ router.get('/', (req, res) => {
   res.send([]);
 });
 
+router.post('/', (req, res) => {
+  res.status(201).send({
+    id: 1,
+    name: 'Jordan 1 Retro'
+  });
+});
+
 module.exports = router;
--- tests/routes/shoes.test.js
+++ tests/routes/shoes.test.js
@@ -7,3 +7,16 @@ test('GET /shoes starts empty', async () => {
   expect(res.statusCode).toBe(200);
   expect(res.body).toEqual([]);
 });
+
+test('POST /shoes creates a shoe entry with an id', async () => {
+  const shoe = {name: 'Jordan 1 Retro'};
+  const res = await request(app).post('/shoes', shoe);
+
+  expect(res.statusCode).toBe(201);
+
+  const id = res.body.id;
+  expect(Number.isInteger(id)).toBe(true);
+  expect(id).toBeGreaterThan(0);
+
+  expect(res.body.name).toBe('Jordan 1 Retro');
+});
